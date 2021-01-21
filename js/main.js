196c8a15d15b56476c7310c0899f4cb7691a5330
EvgK-2020 <69770881+EvgK-2020@users.noreply.github.com>
2021-01-21 04:27:05 +0300

    Lesson-3

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ class Product {
                   <div class="desc">
                       <h3>${this.title}</h3>
                       <p>${this.price}</p>
-                      <button class="buy-btn">Купить</button>
+                      <button class="buy-btn" data-product_id="${this.id}">Купить</button>
                   </div>
               </div>`
     }
@@ -28,10 +28,10 @@ class ProductsList {
         this.container = document.querySelector(container);
         this._fetchData();
         this._render();
-        this._costProductsList()
+        console.log(`Стоимость всех товаров: ${this._costProductsList()}`);
     }
 
-    init() {}
+    init() { }
 
     _fetchData() {
         this.data = [
@@ -52,18 +52,20 @@ class ProductsList {
 
     _costProductsList() {
         let cost = 0;
-        for (let dataEl of this.products){
+        for (let dataEl of this.products) {
             cost += dataEl.price;
         }
-        console.log(`Стоимость всех товаров: ${cost}`);
+        return cost;
     }
 }
 
-const list = new ProductsList();
-
-
-class CartItem{
-    constructor(){
+class CartItem {
+    constructor(product, quantity) {
+        let { id, title, price = 0 } = product;
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
         // id - id товара
         // title - название товара
         // img - картинка товара
@@ -72,18 +74,144 @@ class CartItem{
     }
 
     //changeQuantity() - метод для изменения количества товара
-    //render() - метод для построения и вывода товарной позиции
-}
+    render() {
+        return `<div class="cart_item_product" id="cart_item_product_${this.id}">
+        <div class="cart_item_product_name">${this.title}</div>
+        <div class="cart_item_product_quantity_minus plus_minus" data-id="${this.id}" data-action="minus" title="убрать 1">	
+        &#10134;</div>
+        <div class="cart_item_product_quantity">${this.quantity}</div>
+         <div class="cart_item_product_quantity_plus plus_minus data-id="${this.id}" data-action="plus" title="добавить 1">	
+        &#10133;</div> 
+        <div class="cart_item_product_price">${this.price}</div>
+        <div class="cart_item_product_cost">${this.quantity * this.price}</div>
+    </div>`;
+    }
 
+    changeQuantity(e){
+        console.log(e.target);
+    }
+
+
+}
 
 class Cart {
     constructor() {
-        // data - массив товаров в корзине
-        // container - куда выводить корзину
+        this.productInCart = [];
+//        this._costOfCartItems();  // - метод для подсчета стоимости товаров в корзине
     }
 
-    // fetchData() - метод для получения списка товаров для корзины
-    // costOfCartItems () - метод для подсчета стоимости товаров в корзине
-    // render() - метод для построения и вывода корзины
-    // delItem() - метод для удаления позиции из корзины
+ 
+    addToCart(e) {
+        this.productInCart.push(Number(e.target.dataset.product_id));
+        alert("Товар добавлен в корзину");
+    }
+
+    removeFromCart(e) {
+        this.productInCart = this.productInCart.filter(item => item != e.target.dataset.id);
+        console.log(this.productInCart);
+        this._closeCart();
+        this.render();
+    }
+
+    render() {       // - метод для построения и вывода корзины
+        this._renderCart();
+        this._renderCartItems();
+        wrp_cart.insertAdjacentHTML('beforeend', `Итого: ${this._costOfCartItems()}`);
+        this._renderRemoveButton();
+    }
+
+    _renderCart() {
+        let darkLayer = document.createElement('div');
+        darkLayer.id = 'shadow';
+        document.body.appendChild(darkLayer);
+
+        let modalWin = document.getElementById('modal');
+        modalWin.classList.remove("close");
+
+       
+        darkLayer.onclick = function () {
+            darkLayer.parentNode.removeChild(darkLayer);
+            modalWin.classList.add("close");
+        };
+    };
+
+    _renderCartItems() {
+        let countProducts = {};
+        for (let elem of this.productInCart) {
+            if (countProducts[elem] === undefined) {
+                countProducts[elem] = 1;
+            }
+            else {
+                countProducts[elem]++;
+            }
+        }
+
+        let allProductItem = `<div class="cart_item_product">
+        <div class="cart_item_product_name">Название</div>
+        <div class="cart_item_product_quantity">Количество</div>
+        <div class="cart_item_product_price">Цена</div>
+        <div class="cart_item_product_cost">Стоимость</div>
+    </div>`;
+        for (let dataEl in countProducts) {
+            const productItem = new CartItem(showcase.products.find(item => item.id == dataEl), countProducts[dataEl]);
+            allProductItem += productItem.render();
+        }
+        wrp_cart.innerHTML = allProductItem;
+        
+    }
+
+    _costOfCartItems() {
+        let totalCost = 0;
+        for (let dataEl of this.productInCart) {
+            totalCost += showcase.products.find(item => item.id == dataEl).price;
+        }
+        return totalCost;
+    };
+
+    _renderRemoveButton() {
+        for (let dataEl of Array.from(new Set(this.productInCart))) {
+            document.getElementById("cart_item_product_" + dataEl).insertAdjacentHTML('beforeend', `<div class="remove-product"  data-id="${dataEl}"title="Удалить позицию">&#128465;</div>`);
+            document.querySelector(".remove-product").onclick = removeFromCart;
+        }
+    };
+
+    _closeCart(dLayer="shadow",mlWin = "modal") {
+        let darkLayer =document.getElementById(dLayer);
+        let modalWin = document.getElementById(mlWin);
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.classList.add("close");
+    };
+
 }
+
+
+
+
+
+const showcase = new ProductsList();
+
+
+let buttons = document.querySelectorAll(".buy-btn");
+for (let item of buttons) {
+    item.onclick = addToCart;
+}
+document.querySelector(".btn-cart").onclick = showCart;
+
+
+function addToCart(e) {
+    cart.addToCart(e);
+}
+
+function removeFromCart(e) {
+    cart.removeFromCart(e);
+}
+
+function changeQuantity(e){
+    console.log("тыц");
+}
+
+function showCart() {
+    cart.render();
+}
+
+const cart = new Cart();
